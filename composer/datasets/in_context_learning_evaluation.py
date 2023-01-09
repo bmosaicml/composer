3ac521f4b66d4c5b19b516562032350f948c34f3
bmosaicml <jeremy@mosaicml.com>
2023-01-09 17:10:57 -0500

    merge

--- composer/datasets/in_context_learning_evaluation.py
+++ composer/datasets/in_context_learning_evaluation.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import random
-import textwrap
 
 import torch
 from datasets import load_dataset
@@ -10,7 +9,7 @@ from torch.utils.data import DataLoader, Dataset
 from transformers import AutoTokenizer
 
 from composer.core import DataSpec
-from composer.utils import dist, ensure_tuple
+from composer.utils import dist
 from composer.utils.file_helpers import get_file
 
 
@@ -116,30 +115,7 @@ class InContextLearningLMTaskDataset(Dataset):
         }
 
     def get_num_samples_in_batch(self, batch) -> int:
-        if isinstance(batch, torch.Tensor):
-            return batch.shape[0]
-
-        dim0_sizes = []
-        if isinstance(batch, (list, tuple)):
-            for tensors in batch:
-                for t in ensure_tuple(tensors):
-                    if not hasattr(t, 'shape'):
-                        raise ValueError('Unable to determine the batch size, batch contains'
-                                         f'an element of type {type(t)}, which does not have a'
-                                         'shape. Please use a DataSpec and provide a'
-                                         '`get_num_samples_in_batch(your_batch) -> int` method.')
-                    dim0_sizes.append(t.shape[0])
-        elif isinstance(batch, dict):
-            dim0_sizes = [t.shape[0] for t in batch.values() if isinstance(t, torch.Tensor)]
-
-        if len(set(dim0_sizes)) == 1:
-            return dim0_sizes[0]
-        else:
-            raise NotImplementedError(
-                textwrap.dedent(f"""\
-                    Cannot determine the batch size, as multiple Tensors of
-                    different lengths were found in the batch: sizes in batch: {dim0_sizes}.
-                    Please use a DataSpec and specify `get_num_samples_in_batch`."""))
+        return batch['input_ids'].shape[0]
 
     def update_metric(self, metric, batch, output_logits, labels):
         metric.update(batch, output_logits, labels)
--- tests/datasets/test_in_context_learning_eval_datasets.py
+++ tests/datasets/test_in_context_learning_eval_datasets.py
@@ -13,69 +13,37 @@ from composer.models.gpt2 import create_gpt2
 from composer.trainer import Trainer
 from tests.common import device, world_size
 
-
 @pytest.mark.parametrize('dataset_uri', ['lambada_small.jsonz'])
-def test_get_lm_task_dataloader(dataset_uri):
-    in_memory_logger = InMemoryLogger()  # track the logged metrics in the in_memory_logger
+def test_lm_task_dataloader(dataset_uri, tiny_gpt2_tokenizer):
     local_data = os.path.join(os.path.dirname(__file__), 'local_data')
-    dataset_uri = f'{local_data}/{dataset_uri}'
-    tokenizer = transformers.AutoTokenizer.from_pretrained('gpt2')
-    dl = get_lm_task_dataloader(dataset_uri,
-                                tokenizer,
-                                2,
-                                max_seq_len=2048,
-                                eos_tok_id=tokenizer.eos_token_id,
-                                num_fewshot=0,
-                                preamble_string='',
-                                example_delimiter='\n',
-                                continuation_delimiter='')
-    evaluator = Evaluator(label='lambada', dataloader=dl, metric_names=['InContextLearningLMAccuracy'])
-    model = create_gpt2(use_pretrained=True, pretrained_model_name='EleutherAI/gpt-neo-125M')
-    trainer = Trainer(model=model, max_duration='1ep', loggers=in_memory_logger)
-    trainer.eval(eval_dataloader=evaluator)
-    assert 'metrics/lambada/InContextLearningLMAccuracy' in in_memory_logger.data.keys()
-    assert in_memory_logger.data['metrics/lambada/InContextLearningLMAccuracy'][0][1].item() == 0.4375
 
-
-@pytest.mark.parametrize('dataset_uri', ['lambada_small.jsonz'])
-@pytest.mark.parametrize('num_fewshot', [1, 5, 10])
-def test_get_lm_task_dataloader_fewshot(dataset_uri, num_fewshot):
-    in_memory_logger = InMemoryLogger()  # track the logged metrics in the in_memory_logger
-    local_data = os.path.join(os.path.dirname(__file__), 'local_data')
+    tokenizer = tiny_gpt2_tokenizer
     dataset_uri = f'{local_data}/{dataset_uri}'
-    tokenizer = transformers.AutoTokenizer.from_pretrained('gpt2')
-    dl = get_lm_task_dataloader(dataset_uri,
-                                tokenizer,
-                                2,
-                                max_seq_len=2048,
-                                eos_tok_id=tokenizer.eos_token_id,
-                                num_fewshot=num_fewshot,
-                                preamble_string='',
-                                example_delimiter='\n',
-                                continuation_delimiter='')
-    evaluator = Evaluator(label='lambada', dataloader=dl, metric_names=['InContextLearningLMAccuracy'])
-    model = create_gpt2(use_pretrained=True, pretrained_model_name='EleutherAI/gpt-neo-125M')
-    trainer = Trainer(model=model, max_duration='1ep', loggers=in_memory_logger)
-    trainer.eval(eval_dataloader=evaluator)
-    assert 'metrics/lambada/InContextLearningLMAccuracy' in in_memory_logger.data.keys()
-    assert in_memory_logger.data['metrics/lambada/InContextLearningLMAccuracy'][0][1].item() >= 0.125
+    dl = get_lm_task_dataloader(dataset_uri, tokenizer, 2, max_seq_len=2048, eos_tok_id=tokenizer.eos_token_id)
+
+    assert 'input_ids' in next(dl.dataloader._get_iterator())
+    assert 'attention_mask' in next(dl.dataloader._get_iterator())
+    assert 'continuation_indices' in next(dl.dataloader._get_iterator())
+    assert 'labels' in next(dl.dataloader._get_iterator())
+    assert 'mode' in next(dl.dataloader._get_iterator())
 
 
 @pytest.mark.parametrize('dataset_uri', ['lambada_small.jsonz'])
 @world_size(1, 2)
-@device('gpu')
-def test_get_lm_task_dataloader_gpu(device, world_size, dataset_uri):
+@device('gpu', 'cpu')
+@pytest.mark.parametrize('num_fewshot', [0, 1, 5])
+def test_get_lm_task_dataloader(device, world_size, dataset_uri, num_fewshot, tiny_gpt2_tokenizer):
     in_memory_logger = InMemoryLogger()  # track the logged metrics in the in_memory_logger
     local_data = os.path.join(os.path.dirname(__file__), 'local_data')
     dataset_uri = f'{local_data}/{dataset_uri}'
-    tokenizer = transformers.AutoTokenizer.from_pretrained('gpt2')
+    tokenizer = tiny_gpt2_tokenizer
     dl = get_lm_task_dataloader(
         dataset_uri,
         tokenizer,
         2,
         max_seq_len=2048,
         eos_tok_id=tokenizer.eos_token_id,
-        num_fewshot=0,
+        num_fewshot=num_fewshot,
         preamble_string="",
         example_delimiter='\n',
         continuation_delimiter=""
@@ -85,4 +53,4 @@ def test_get_lm_task_dataloader_gpu(device, world_size, dataset_uri):
     trainer = Trainer(model=model, max_duration='1ep', loggers=in_memory_logger)
     trainer.eval(eval_dataloader=evaluator)
     assert 'metrics/lambada/InContextLearningLMAccuracy' in in_memory_logger.data.keys()
-    assert in_memory_logger.data['metrics/lambada/InContextLearningLMAccuracy'][0][1].item() == 0.4375
+    assert in_memory_logger.data['metrics/lambada/InContextLearningLMAccuracy'][0][1].item() >= 0.125
